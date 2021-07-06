c64fc880da81554da265c7a15ca5cc3af69e0e5d
chaejinho <asdzxc9395@naver.com>
2021-07-06 17:00:32 +0900

    spring core 2day

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,2 +1,22 @@
-package hello.core.member;public class MemberServiceTest {
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given ==>이런 일이 주어졌을때 이렇게 된다.
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
 }
